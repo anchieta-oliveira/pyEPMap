861c504df91d301e4e91083564f4fd28d0ace28f
Anchieta Oliveira <anchieta-13@hotmail.com>
2025-02-09 14:23:53 -0300

    nov bplot de barra

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -28,7 +28,7 @@ def run_trp_cage():
     gps_cpu = []
     gs = 1
 
-    while gs >= 0.1:
+    while gs >= 0.4:
         print(f"EPMap res: {gs}")
         start_time = time.time() 
         dx = epm.calculate(pdb=pdb, charges=psf.charges, res=gs, gpu=False, margim=0.3, cutoff=30, gpus_id=[0], form="dx")
@@ -38,13 +38,14 @@ def run_trp_cage():
 
         ts_cpu.append(t)
         gps_cpu.append(gp)
-        print(f"Grid spacing {t} s - {gp} grid points")
+        print(f"Time {t} s - {gp} grid points")
         gs -= 0.1
+    #dx.write("fim_cpu.dx")
 
     ts_gpu = []
     gps_gpu = []
     gs = 1
-    while gs >= 0.1:
+    while gs >= 0.4:
         print(f"EPMap res: {gs}")
         start_time = time.time() 
         dx = epm.calculate(pdb=pdb, charges=psf.charges, res=gs, gpu=True, margim=0.3, cutoff=30, gpus_id=[0], form="dx")
@@ -54,9 +55,9 @@ def run_trp_cage():
 
         ts_gpu.append(t)
         gps_gpu.append(gp)
-        print(f"Grid spacing {t} s - {gp} grid points")
+        print(f"Time {t} s - {gp} grid points")
         gs -= 0.1
-
+    #dx.write("fim_gpu.dx")
     
     plt.figure(figsize=(10,6))
     sns.set(style="whitegrid")
@@ -72,8 +73,43 @@ def run_trp_cage():
     plt.gca().xaxis.set_major_formatter(mticker.FuncFormatter(lambda x, _: f'{x:.1f}M'))
 
     plt.legend()
-    plt.savefig("1o0h_cpu_gpu.png", dpi=250)
-    plt.show()
+    plt.savefig("2luf_cpu_gpu.png", dpi=250)
+    #plt.show()
+
+    plt.close()
+
+    # Calcula aceleração GPU/CPU
+    speedup = np.array(ts_cpu) / np.array(ts_gpu)
+
+    # Configurações do gráfico
+    plt.figure(figsize=(10, 6))
+    sns.set(style="whitegrid")
+
+    # Criando deslocamento para separar as barras
+    x = np.arange(len(gps_cpu))  # Índices das barras
+    width = 0.4  # Largura das barras
+
+    # Plotando as barras
+    plt.bar(x - width/2, ts_cpu, width, label='CPU', color='b')
+    plt.bar(x + width/2, ts_gpu, width, label='GPU', color='r')
+
+
+    for i in range(len(speedup)):
+        plt.text( (x[i] - width/2 + x[i] + width/2) / 2, ts_cpu[i] * 1.01,  
+                f'×{speedup[i]:.1f}', 
+                ha='center', va='bottom', fontsize=10,
+                color='black', fontweight='bold')
+        
+   
+    plt.xticks(x, [f"{gp/1e6:.1f}M" for gp in gps_cpu])  
+    plt.xlabel('Number of Grid Points (millions)')
+    plt.ylabel('Execution Time (seconds)')
+    plt.title('Benchmark: Execution Time (CPU vs GPU)')
+
+    plt.legend()
+    plt.savefig("2luf_cpu_gpu_bar.png", dpi=250)
+    #plt.show()
+
 
 
 def run_8y3c():
@@ -98,6 +134,7 @@ def run_8y3c():
         gps_cpu.append(gp)
         print(f"Grid spacing {t} s - {gp} grid points")
         gs -= 0.1
+    #dx.write(f"1o0h_gs{gs}_cpu_mep.dx")
 
     ts_gpu = []
     gps_gpu = []
@@ -115,7 +152,7 @@ def run_8y3c():
         print(f"Grid spacing {t} s - {gp} grid points")
         gs -= 0.1
 
-    dx.write(f"1o0h_gs{gs}_gpu_mep.dx")
+    #dx.write(f"1o0h_gs{gs}_gpu_mep.dx")
     
     plt.figure(figsize=(10,6))
     sns.set(style="whitegrid")
@@ -132,9 +169,40 @@ def run_8y3c():
 
     plt.legend()
     plt.savefig("1o0h_cpu_gpu.png", dpi=250)
-    plt.show()
 
+    plt.close()
+
+    # Calcula aceleração GPU/CPU
+    speedup = np.array(ts_cpu) / np.array(ts_gpu)
+
+    # Configurações do gráfico
+    plt.figure(figsize=(10, 6))
+    sns.set(style="whitegrid")
+
+    # Criando deslocamento para separar as barras
+    x = np.arange(len(gps_cpu))  # Índices das barras
+    width = 0.4  # Largura das barras
+
+    # Plotando as barras
+    plt.bar(x - width/2, ts_cpu, width, label='CPU', color='b')
+    plt.bar(x + width/2, ts_gpu, width, label='GPU', color='r')
+
+
+    for i in range(len(speedup)):
+        plt.text( (x[i] - width/2 + x[i] + width/2) / 2, ts_cpu[i] * 1.01,  
+                f'×{speedup[i]:.1f}', 
+                ha='center', va='bottom', fontsize=10,
+                color='black', fontweight='bold')
+        
+   
+    plt.xticks(x, [f"{gp/1e6:.1f}M" for gp in gps_cpu])  
+    plt.xlabel('Number of Grid Points (millions)')
+    plt.ylabel('Execution Time (seconds)')
+    plt.title('Benchmark: Execution Time (CPU vs GPU)')
+
+    plt.legend()
+    plt.savefig("1o0h_cpu_gpu_bar.png", dpi=250)
 
 # Run...
-#run_trp_cage()
-run_8y3c()
+run_trp_cage()
+#run_8y3c()
